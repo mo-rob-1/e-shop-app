eb9419dc2c159ddc36ead7d4cf1542ce90015a0c
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-06-08 18:42:06 +0100

    add product model

---
+++ backend/models/productModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  description: {
+    type: String,
+    required: [true, "Description is required"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Price is required"],
+  },
+  countInStock: {
+    type: Number,
+    required: [true, "Count in stock is required"],
+  },
+});
+
+module.exports = mongoose.model("Product", ProductSchema);
