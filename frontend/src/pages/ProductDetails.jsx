4695b51e76b3f49c80b2ec0dbc007d00d33cad7a
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-06-10 20:39:49 +0100

    add functionality

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,5 +1,80 @@
-function ProductDetails() {
-  return <div>ProductDetails</div>;
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+
+// Actions
+import { getProductsDetails } from "../redux/actions/productActions";
+import { addToCart } from "../redux/actions/cartActions";
+
+function ProductDetails({ history }) {
+  const [qty, setQty] = useState(1);
+
+  const dispatch = useDispatch();
+
+  const { id } = useParams();
+
+  const productDetails = useSelector((state) => state.getProductDetails);
+  const { loading, error, product } = productDetails;
+
+  useEffect(() => {
+    dispatch(getProductsDetails(id));
+  }, [dispatch, id]);
+
+  const addToCartHandler = () => {
+    dispatch(addToCart(product._id, qty));
+    history.push(`/cart`);
+  };
+
+  return (
+    <div>
+      {" "}
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h2>{error}</h2>
+      ) : (
+        <>
+          <div>
+            <div>
+              <img src={product.imageUrl} alt={product.name} />
+            </div>
+            <div>
+              <p>{product.name}</p>
+              <p>Price: ${product.price}</p>
+              <p>Description: {product.description}</p>
+            </div>
+          </div>
+          <div>
+            <div>
+              <p>
+                Price:
+                <span>${product.price}</span>
+              </p>
+              <p>
+                Status:
+                <span>{product.countInStock > 0 ? "In Stock" : "Out of Stock"}</span>
+              </p>
+              <p>
+                Qty
+                <select value={qty} onChange={(e) => setQty(e.target.value)}>
+                  {[...Array(product.countInStock).keys()].map((x) => (
+                    <option key={x + 1} value={x + 1}>
+                      {x + 1}
+                    </option>
+                  ))}
+                </select>
+              </p>
+              <p>
+                <button type="button" onClick={addToCartHandler}>
+                  Add To Cart
+                </button>
+              </p>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default ProductDetails;
