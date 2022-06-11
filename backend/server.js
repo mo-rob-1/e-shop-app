62f56d2075cd904f1822e86ec7e53ef6fd2b36e0
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-06-11 18:00:55 +0100

    deploy to heroku

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/products", require("./routes/productRoutes"));
 
+// Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({ message: "Welcome to the e-Shop API" });
+  });
+}
+
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
