93563938d5f6a95b1f8f3f805ed3e095fd6e2a9b
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-06-17 17:41:36 +0100

    add login and register buttons

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,66 @@
-import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../redux/features/auth/authSlice";
 
 function Header() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.auth);
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  const getCartCount = () => {
+    return cartItems.reduce((qty, item) => Number(item.qty) + qty, 0);
+  };
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    // localStorage.removeItem("cart");
+    navigate("/");
+    // window.location.reload();
+  };
+
   return (
-    <div>
-      <h1>e-Shop</h1>
-    </div>
+    <header>
+      <div>
+        <div>
+          <Link to="/">e-Shop</Link>
+        </div>
+      </div>
+      <ul>
+        {user ? (
+          <>
+            {/* // show name of user */}
+            <li>Hi {user.name}</li>
+
+            <li>
+              <Link to="/cart">
+                <i className="fas fa-shopping-cart"></i>
+                <span>
+                  Cart <span>{getCartCount()}</span>
+                </span>
+              </Link>
+            </li>
+            <li>
+              <button onClick={onLogout}>Logout</button>
+            </li>
+          </>
+        ) : (
+          <>
+            {" "}
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </header>
   );
 }
 
